70f260fb53e0acef2d401da6190b1f16bfa63208
todd <toddaustin33@gmail.com>
2016-11-27 12:00:26 -0700

    finish js for product listing

--- js/products.js
+++ js/products.js
@@ -12,10 +12,13 @@ var items = {
     "Hex": "17.95",
     "Byte": "18.95"
 };
+
+
 var addToCart = document.getElementsByClassName('add');
 var removeFromCart = document.getElementsByClassName('remove');
 var cartCont = document.getElementsByClassName('tooltip-text');
 var cartQty = document.getElementsByClassName('qty');
+var cartItem = document.getElementsByClassName('item');
 var cartItemPrice = document.getElementsByClassName('cart-price');
 var cartTotal = document.getElementsByClassName('amount');
 var promo = document.getElementById('apply-code');
@@ -24,6 +27,38 @@ addItem();
 removeItem();
 applyPromo();
 
+
+function nonDiscountTotal() {
+    var tpi = 0;
+    for (var x = 0; x < cartItem.length; x++) {
+        var cartItemQty = parseInt(cartItem[x].nextElementSibling.value);
+        var itemPrice = parseFloat(items[cartItem[x].textContent]);;
+        tpi += parseFloat(itemPrice * cartItemQty);
+
+    }
+   
+   return tpi.toFixed(2);
+
+
+}
+
+var cart = {
+    'updateQty' : function() {
+        var qtyVal = 0;
+        for (var j = 0; j < cartQty.length; j++) {
+            qtyVal += parseInt(cartQty[j].value);
+        }
+        return qtyVal;
+    },
+    'updateCartTotal' : function() {
+        for (var j = 0; j < cartQty.length; j++) {
+            totalVal += parseFloat(cartItemPrice[j].innerHTML.slice(1));
+        }
+    },
+    'addToCart2' : function(){
+
+    }
+};
 
 function updateCartCount() {
     var qtyVal = 0;
@@ -43,7 +78,9 @@ function removeItemFromCart(that) {
 
 function addItem() {
     for (var i = 0; i < addToCart.length; i++) {
+
         addToCart[i].addEventListener('click', function() {
+            var cartChild = cartCont[0].children.length;
             var curProdName = this.parentElement.firstElementChild.firstElementChild.textContent;
             var curProdPrice = items[curProdName];
             var curProdQuantity = this.previousElementSibling.firstElementChild.value;
@@ -105,9 +142,10 @@ function updateItemQty() {
 
 function applyPromo() {
     promo.addEventListener('click', function() {
+       var ndt = nonDiscountTotal();
         var pCode = this.previousElementSibling.value.toLowerCase();
-        if (pCode === "10off") {
-            // 10% off one item
+        if (pCode !== ""){
+            updateCartCount();
             var cartChild = cartCont[0].children;
             var priceCheck = 0;
             for (var p = 0; p < cartChild.length; p++) {
@@ -116,18 +154,59 @@ function applyPromo() {
                     priceCheck = parseFloat(items[iName]);
                 }
             }
-            var tenOff = parseFloat(priceCheck * 0.10).toFixed(2);
-            var newT = parseFloat(cartTotal[0].textContent.slice(1) - tenOff);
-            cartTotal[0].textContent = "$" + newT.toFixed(2);
-            priceCheck = 0;
-            this.previousElementSibling.value = "";
-      
-        } else if (pCode === "train15") {
-            // 15 off Train shirts
-       
-        } else if (pCode === "5offtotal") {
-            // 5% off total
-            cartTotal[0].textContent = "$" + parseFloat(cartTotal[0].textContent.slice(1) - (cartTotal[0].textContent.slice(1) * 0.05)).toFixed(2);
+
+            if (pCode === "10off") {
+                // 10% off one item
+                var tenOff = parseFloat(priceCheck * 0.10).toFixed(2);
+                var newT = parseFloat((cartTotal[0].textContent.slice(1) - tenOff).toFixed(2));
+                console.log(typeof newT);
+                if (parseFloat(cartTotal[0].textContent.slice(1)) > newT){
+                    cartTotal[0].textContent = "$" + newT.toFixed(2);
+                    priceCheck = 0;
+                    this.previousElementSibling.value = "";
+                } else {
+                    alert('You currently have the best price.');
+                }
+          
+            } else if (pCode === "train15") {
+                // 15 off Train shirts
+                var discount = 0;
+                var qtyToDiscount = 0;
+                for (var t = 0; t < cartChild.length; t++) {
+                    var iName = cartChild[t].firstChild.textContent;
+                    if (iName === 'Train'){
+                        qtyToDiscount = parseInt(cartChild[t].firstChild.nextElementSibling.value);
+                        var d = parseFloat(items['Train'] * 0.15).toFixed(2);
+                        discount = qtyToDiscount * d; 
+                        currentTrainTotal = parseFloat(items['Train'] * qtyToDiscount);
+                        cartChild[t].firstChild.nextElementSibling.nextElementSibling.textContent = "$" + (currentTrainTotal - parseFloat(discount.toFixed(2)));
+                    }
+                }
+                if (qtyToDiscount !== 0){
+                    var cartItemsTotal = 0;
+                    for (var c = 0; c < cartItemPrice.length; c++) {
+                       cartItemsTotal += parseFloat(cartItemPrice[c].textContent.slice(1)).toFixed(2);
+                    }
+                    cartItemsTotal = parseFloat(cartItemsTotal);
+                    if (cartTotal[0].textContent.slice(1) > cartItemsTotal) {
+                        cartTotal[0].textContent = "$" + cartItemsTotal;
+                        priceCheck = 0;
+                        this.previousElementSibling.value = "";
+                    } else {
+                        alert('You currently have the best price.');
+                    }
+                } else {
+                    alert("Promo code not valid for items in cart");
+                }
+            
+            updateCartCount();
+            } else if (pCode === "5offtotal") {
+                // 5% off total
+                updateCartCount();
+                cartTotal[0].textContent = "$" + parseFloat(cartTotal[0].textContent.slice(1) - (cartTotal[0].textContent.slice(1) * 0.05)).toFixed(2);
+            } else {
+                alert('Promo code is invalid');
+            }
         }
     });
 }
--- js/testing/products-works.js
+++ js/testing/products-works.js
@@ -12,20 +12,17 @@ var items = {
     "Hex": "17.95",
     "Byte": "18.95"
 };
-
-
 var addToCart = document.getElementsByClassName('add');
 var removeFromCart = document.getElementsByClassName('remove');
 var cartCont = document.getElementsByClassName('tooltip-text');
 var cartQty = document.getElementsByClassName('qty');
 var cartItemPrice = document.getElementsByClassName('cart-price');
 var cartTotal = document.getElementsByClassName('amount');
-// var promo = document.getElementById('apply-code');
-
+var promo = document.getElementById('apply-code');
 
 addItem();
- removeItem();
-// applyPromo();
+removeItem();
+applyPromo();
 
 
 function updateCartCount() {
@@ -34,9 +31,7 @@ function updateCartCount() {
     for (var j = 0; j < cartQty.length; j++) {
         qtyVal += parseInt(cartQty[j].value);
         totalVal += parseFloat(cartItemPrice[j].innerHTML.slice(1));
-        console.log(totalVal);
     }
-    console.log(totalVal);
     document.getElementsByClassName('cart-count')[0].innerHTML = qtyVal;
     cartTotal[0].innerHTML = "$" + totalVal.toFixed(2);
 }
@@ -47,16 +42,31 @@ function removeItemFromCart(that) {
 }
 
 function addItem() {
-    for (var n = 0; n < addToCart.length; n++) {
-        addToCart[n].addEventListener('click', function() {
+    for (var i = 0; i < addToCart.length; i++) {
+        addToCart[i].addEventListener('click', function() {
             var curProdName = this.parentElement.firstElementChild.firstElementChild.textContent;
             var curProdPrice = items[curProdName];
             var curProdQuantity = this.previousElementSibling.firstElementChild.value;
-            var totalPrice = parseFloat(curProdPrice * curProdQuantity).toFixed(2);
+            var cartChild = cartCont[0].children;
+            var inCart = false;
+            for (var c = 2; c < cartChild.length; c++) {
+                var currentEntry = cartChild[c].firstElementChild;
+                if (currentEntry.textContent === curProdName){
+                    var curValue = parseInt(currentEntry.nextElementSibling.value);
+                    curValue = curValue + parseInt(curProdQuantity);
+                    currentEntry.nextElementSibling.value = curValue;
+                    currentEntry.nextElementSibling.nextElementSibling.textContent = '$' + parseFloat(curValue * curProdPrice).toFixed(2);
+                   inCart = true;
+                }
+            } 
+          
+          if (!inCart){
+            var totalPrice = (curProdPrice * curProdQuantity).toFixed(2);
             var newP = document.createElement('p');
             newP.innerHTML = "<span class=\"item\">" + curProdName + "</span> <input class=\"qty\" value=" + curProdQuantity + "> <span class=\"cart-price\">$" + totalPrice + "</span><button class=\"remove\">Remove</button>";
 
             cartCont[0].appendChild(newP);
+        }
             removeItem();
             updateCartCount();
             updateItemQty();
@@ -85,25 +95,44 @@ function updateItemQty() {
             if (curAmount == 0) {
                 removeItemFromCart(curItem);
             } else {
-                var newAmount = parseFloat(curItemPrice * curAmount).toFixed(2);
-                updatePrice[0].innerHTML = "$" + newAmount;
+                var newAmount = parseFloat(curItemPrice * curAmount);
+                updatePrice[0].innerHTML = "$" + newAmount.toFixed(2);
                 updateCartCount();
             }
         });
     }
 }
 
-// function applyPromo() {
-//     promo.addEventListener('click', function() {
-//         var pCode = this.previousElementSibling.value.toLowerCase();
-//         if (pCode == "10off") {
-//             // 10% off one item
+function applyPromo() {
+    promo.addEventListener('click', function() {
 
-//         } else if (pCode == "train15") {
-//             // 15 off Train shirts
-
-//         } else if (pCode == "5offtotal") {
-//             // 5% off total
-//         }
-//     });
-// }
+        var pCode = this.previousElementSibling.value.toLowerCase();
+        if (pCode !== ""){
+            if (pCode === "10off") {
+                // 10% off one item
+                updateCartCount();
+                var cartChild = cartCont[0].children;
+                var priceCheck = 0;
+                for (var p = 0; p < cartChild.length; p++) {
+                    var iName = cartChild[p].firstChild.textContent;
+                    if (items[iName] >= priceCheck) {
+                        priceCheck = parseFloat(items[iName]);
+                    }
+                }
+                var tenOff = parseFloat(priceCheck * 0.10).toFixed(2);
+                var newT = parseFloat(cartTotal[0].textContent.slice(1) - tenOff);
+                cartTotal[0].textContent = "$" + newT.toFixed(2);
+                priceCheck = 0;
+                this.previousElementSibling.value = "";
+          
+            } else if (pCode === "train15") {
+                // 15 off Train shirts
+            updateCartCount();
+            } else if (pCode === "5offtotal") {
+                // 5% off total
+                updateCartCount();
+                cartTotal[0].textContent = "$" + parseFloat(cartTotal[0].textContent.slice(1) - (cartTotal[0].textContent.slice(1) * 0.05)).toFixed(2);
+            }
+        }
+    });
+}
